a80ad6059f290fd0a60790ccf8ddc9f930bf1d25
ayan0508 <64884225+ayan0508@users.noreply.github.com>
2022-06-07 00:21:20 +0530

    Add files via upload

---
+++ CRC_rec.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+string subtract(string res,string div)
+{
+    int l= div.size();
+    if(res[0]=='0')
+        return res;
+    for(int i=l;i>=0;i--)
+    {
+        int p = (int)res[i] ^ (int)div[i];
+        res[i] = '0'+p;
+    }
+    return res;
+}
+void receverside(string data,string div)
+{
+    int l1 = data.size();
+    int l2 = div.size();
+    string res = "";
+    string  ch= "";
+    res+=data.substr(0,l2);
+    for(int i =l2;i<=l1;i++)
+    {
+       res =subtract(res,div);
+       if(res[0]=='0')
+       {
+           res.erase(0,1);
+            res+=data[i];
+       }
+       else
+        continue;
+    }
+    cout<<res;
+}
+int main()
+{
+    string data;
+    cout<<"Enter the data"<<endl;
+    cin>>data;
+    string div;
+    cout<<"Enter the divisor"<<endl;
+    cin>>div;
+    receverside(data,div);
+}
